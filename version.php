c85889c2112d5c7e47de0331c5f89759d2889e5b
Eloy Lafuente (stronk7) <stronk7>
2007-04-01 22:57:16 +0000

    bump version to test upgrade example

--- version.php
+++ version.php
@@ -1,4 +1,5 @@
 <?php // $Id$
+
 /**
  * Code fragment to define the version of newmodule
  * This fragment is called by moodle_needs_upgrading() and /admin/index.php
@@ -8,7 +9,7 @@
  * @package newmodule
  **/
 
-$module->version  = 2007022800;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2007040200;  // The current module version (Date: YYYYMMDDXX)
 $module->cron     = 0;           // Period for cron to check this module (secs)
 
 ?>
