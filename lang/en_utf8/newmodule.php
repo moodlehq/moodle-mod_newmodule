7ad8045de408ac89358ba35210db07713c3333bb
Eloy Lafuente (stronk7) <stronk7>
2007-04-01 11:18:59 +0000

    Adding two missing strings

--- lang/en_utf8/newmodule.php
+++ lang/en_utf8/newmodule.php
@@ -6,5 +6,7 @@ $string['modulename'] = 'newmodule';
 $string['modulenameplural'] = 'newmoduleS';
 
 $string['newmodulefieldset'] = 'Custom example fieldset';
+$string['newmoduleintro'] = 'newmodule Intro';
+$string['newmodulename'] = 'newmodule Name';
 
 ?>
