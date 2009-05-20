bda6e3476b00feb32ba6326f7fb78f60f72dbec6
David Mudrak <david.mudrak@gmail.com>
2009-05-20 21:59:02 +0000

    CONTRIB-1300 Modifications according to the Coding style
    * Standard header for every PHP file containing GPL v3 license note.
    * Removed ?> at the EOF

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,22 +1,44 @@
-<?php  //$Id$
+<?php
 
-// This file keeps track of upgrades to
-// the newmodule module
+// This file is part of Moodle - http://moodle.org/  
+// 
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-// Sometimes, changes between versions involve
-// alterations to database structures and other
-// major things that may break installations.
-//
-// The upgrade function in this file will attempt
-// to perform all the necessary actions to upgrade
-// your older installtion to the current version.
-//
-// If there's something it cannot do itself, it
-// will tell you what you need to do.
-//
-// The commands in here will all be database-neutral,
-// using the functions defined in lib/ddllib.php
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+
+/**
+ * This file keeps track of upgrades to the newmodule module
+ *
+ * Sometimes, changes between versions involve alterations to database 
+ * structures and other major things that may break installations. The upgrade 
+ * function in this file will attempt to perform all the necessary actions to 
+ * upgrade your older installtion to the current version. If there's something 
+ * it cannot do itself, it will tell you what you need to do.  The commands in 
+ * here will all be database-neutral, using the functions defined in 
+ * lib/ddllib.php 
+ *
+ * @package   mod-newmodule
+ * @copyright 2009 Your Name
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+/**
+ * xmldb_newmodule_upgrade 
+ * 
+ * @param int $oldversion 
+ * @return bool
+ */
 function xmldb_newmodule_upgrade($oldversion=0) {
 
     global $CFG, $THEME, $db;
