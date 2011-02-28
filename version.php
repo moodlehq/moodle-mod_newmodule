38ea1e23ea2f9f252aaa1d31166ec1fe4e521680
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-02-28 19:19:14 +0100

    Added ->component to check module is in correct place

--- version.php
+++ version.php
@@ -29,7 +29,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version  = 0;           // If version == 0 then module will not be installed
-//$module->version  = 2010032200;  // The current module version (Date: YYYYMMDDXX)
-$module->requires = 2010031900;  // Requires this Moodle version
-$module->cron     = 0;           // Period for cron to check this module (secs)
+$module->version   = 0;               // If version == 0 then module will not be installed
+//$module->version   = 2010032200;      // The current module version (Date: YYYYMMDDXX)
+$module->requires  = 2010031900;      // Requires this Moodle version
+$module->cron      = 0;               // Period for cron to check this module (secs)
+$module->component = 'mod_newmodule'; // To check on upgrade, that module sits in correct place
