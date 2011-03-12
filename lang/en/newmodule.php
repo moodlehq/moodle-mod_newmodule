812ffcbd96b717c536d304ba1d8142c60fdb9fe9
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2011-03-12 01:52:45 +0100

    MDL-26785 modedit - add one interesting string

--- lang/en/newmodule.php
+++ lang/en/newmodule.php
@@ -31,6 +31,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $string['modulename'] = 'newmodule';
 $string['modulenameplural'] = 'newmodules';
+$string['modulename_help'] = 'Use the newmodule module for... | The newmodule module allows...';
 $string['newmodulefieldset'] = 'Custom example fieldset';
 $string['newmodulename'] = 'newmodule name';
 $string['newmodulename_help'] = 'This is the content of the help tooltip associated with the newmodulename field. Markdown syntax is supported.';
