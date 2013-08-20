2ca5c646cd4be4607e1dad380c796210d24a47a8
Tomasz Muras <tm@enovation.ie>
2013-08-20 22:24:01 +0200

    use __DIR__ and correct # of spaces around =

--- grade.php
+++ grade.php
@@ -23,9 +23,9 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once("../../config.php");
+require_once(__DIR__ . "../../config.php");
 
-$id   = required_param('id', PARAM_INT);          // Course module ID
+$id = required_param('id', PARAM_INT);          // Course module ID
 $itemnumber = optional_param('itemnumber', 0, PARAM_INT); // Item number, may be != 0 for activities that allow more than one grade per user
 $userid = optional_param('userid', 0, PARAM_INT); // Graded user ID (optional)
 
