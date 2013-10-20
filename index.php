b87beded55edabdb60896a2676036116509d4e21
Tomasz Muras <tm@enovation.ie>
2013-10-20 20:39:50 +0200

    html_writer::table accepts html_table object, not stdClass.

--- index.php
+++ index.php
@@ -52,6 +52,7 @@ if (! $newmodules = get_all_instances_in_course('newmodule', $course)) {
     notice(get_string('nonewmodules', 'newmodule'), new moodle_url('/course/view.php', array('id' => $course->id)));
 }
 
+$table = new html_table();
 if ($course->format == 'weeks') {
     $table->head  = array(get_string('week'), get_string('name'));
     $table->align = array('center', 'left');
